5015059a28d4ff096f80182fb0dc7a265476fcc9
pndsdn <saa020403@gmail.com>
2022-03-04 11:00:22 +0300

    changes in test_snp.py

--- test_snp.py
+++ test_snp.py
@@ -1,5 +1,6 @@
 import pytest
-from tornado.httpclient import HTTPClient
+import tornado
+from tornado.httpclient import HTTPClient, AsyncHTTPClient
 
 
 def fetch(url):
@@ -8,9 +9,13 @@ def fetch(url):
     return response.body.decode()
 
 
-assert fetch('http://127.0.0.1:8080/user?name=andrew') == 'Hello, Andrew!'
-assert fetch('http://127.0.0.1:8080/write?message=one,%20two,three,four%20,five-I%60m%20going%20to%20look%21') \
-       == 'One, two, three, four, five - I`m going to look!'
-assert fetch('http://127.0.0.1:8080/write?message=this%20is%20mine%20message.') == 'This is mine message.'
-assert fetch('http://127.0.0.1:8080/write?message=first%20sentence.second%20sentence%20.third%20sentence') \
-       == 'First sentence. Second sentence. Third sentence.'
+def test_snp():
+    assert fetch('http://127.0.0.1:8080/user?name=andrew') == 'Hello, Andrew!'
+    assert fetch('http://127.0.0.1:8080/write?message=one,%20two,three,four%20,five-I%60m%20going%20to%20look%21') \
+           == 'One, two, three, four, five - I`m going to look!'
+    assert fetch('http://127.0.0.1:8080/write?message=this%20is%20mine%20message.') == 'This is mine message.'
+    assert fetch('http://127.0.0.1:8080/write?message=first%20sentence.second%20sentence%20.third%20sentence') \
+           == 'First sentence. Second sentence. Third sentence.'
+
+
+test_snp()
