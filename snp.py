cdfd9aaae0b662ab290e7de03ad3b6cf9e5ed76b
pndsdn <saa020403@gmail.com>
2022-03-10 20:18:16 +0300

    functions have been relocated snp.py --> functional.py

--- snp.py
+++ snp.py
@@ -1,53 +1,17 @@
+from funcional import nameFormating, stringFormating
+
 import tornado.ioloop
 import tornado.web
 
 
 class nameRequestHandler(tornado.web.RequestHandler):
     def get(self):
-        name = self.get_argument('name')
-        self.write(f'Hello, {name[0].capitalize()}{name[1:]}!')
+        nameFormating(self)
 
 
 class stringFormatingRequestHandler(tornado.web.RequestHandler):
     def get(self):
-        message = self.get_argument('message')
-        while '  ' in message:
-            message.replace('  ', ' ')
-
-        while (ord(message[0]) < 65 or ord(message[0]) > 90) and (ord(message[0]) < 97 or ord(message[0]) > 122):
-            message.replace(message[0], '')
-
-        message = f'{message[0].capitalize()}{message[1:]}'
-
-        ch = 0
-        while ch + 2 != len(message):
-            if message[ch] in '.!?':
-                if message[ch+1] != ' ':
-                    message = f'{message[:ch + 1]} {message[ch +1 ].capitalize()}{message[ch + 2:]}'
-
-                if message[ch-1] == ' ':
-                    message = f'{message[:ch - 1]}{message[ch:]}'
-
-            if message[ch] in ',:':
-                if message[ch+1] != ' ':
-                    message = f'{message[:ch + 1]} {message[ch + 1:]}'
-
-                if message[ch-1] == ' ':
-                    message = f'{message[:ch - 1]}{message[ch:]}'
-
-            if message[ch] == '-':
-                if message[ch+1] != ' ':
-                    message = f'{message[:ch + 1]} {message[ch + 1:]}'
-
-                if message[ch-1] != ' ':
-                    message = f'{message[:ch]} {message[ch:]}'
-
-            ch += 1
-
-        if message[len(message)-1] not in '.!?':
-            message = f'{message}.'
-
-        self.write(message)
+        stringFormating(self)
 
 
 app = tornado.web.Application([
