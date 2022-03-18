bfda89a11dedea45a94464ca9ff3b865383e0cf0
pndsdn <saa020403@gmail.com>
2022-03-18 19:50:17 +0300

    minor changes

--- tests/test_snp.py
+++ tests/test_snp.py
@@ -1,19 +1,27 @@
-from tornado.httpclient import HTTPClient
+from abc import ABC
+from collections import namedtuple
+
+import tornado.web
+from tornado.testing import AsyncHTTPTestCase
+
+from src import snp
 
 
-def fetch(url):
-    client = HTTPClient()
-    response = client.fetch(url)
-    return response.body.decode()
+class TestSnpApp(AsyncHTTPTestCase, ABC):
+    def get_app(self) -> tornado.web.Application:
+        return snp.make_app()
 
+    def test_message_request(self):
+        positive_case = namedtuple("positive_case", ["value", "expected"])
+        test_table = [
+            positive_case('one,%20two%20,three%20,four,%20five.I`m%20going%20to%20look%21',
+                          'One, two, three, four, five. I`m going to look!'),
+            positive_case('this%20is%20mine%20message.', 'This is mine message.'),
+            positive_case('first%20sentence.second%20sentence%20.third%20sentence',
+                          'First sentence. Second sentence. Third sentence.')
+        ]
 
-def test_snp():
-    assert fetch('http://127.0.0.1:8081/user?name=andrew') == 'Hello, Andrew!'
-    assert fetch('http://127.0.0.1:8081/write?message=one,%20two,three,four%20,five-I%60m%20going%20to%20look%21') \
-           == 'One, two, three, four, five - I`m going to look!'
-    assert fetch('http://127.0.0.1:8081/write?message=this%20is%20mine%20message.') == 'This is mine message.'
-    assert fetch('http://127.0.0.1:8081/write?message=first%20sentence.second%20sentence%20.third%20sentence') \
-           == 'First sentence. Second sentence. Third sentence.'
-
-
-test_snp()
+        for test_case in test_table:
+            response = self.fetch(f"/write?message=={test_case.value}")
+            self.assertEqual(response.code, 200)
+            self.assertEqual(response.body.decode(), test_case.expected)
